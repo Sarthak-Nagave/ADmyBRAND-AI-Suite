058c16fa73ffd611823339d6b0773ff97307c1bf
sarthak Nagave <sarthaknagave@gmail.com>
2025-07-29 22:24:42 +0530

    Successful build and final changes before deploy

--- server/storage.ts
+++ server/storage.ts
@@ -1,29 +1,32 @@
-import { type User, type InsertUser } from "@shared/schema";
+import { type User, type InsertUser, type Contact, type InsertContact } from "@shared/schema";
 import { randomUUID } from "crypto";
-
-// modify the interface with any CRUD methods
-// you might need
 
 export interface IStorage {
   getUser(id: string): Promise<User | undefined>;
   getUserByUsername(username: string): Promise<User | undefined>;
   createUser(user: InsertUser): Promise<User>;
+
+  getContact(id: string): Promise<Contact | undefined>;
+  createContact(contact: InsertContact): Promise<Contact>;
 }
 
 export class MemStorage implements IStorage {
   private users: Map<string, User>;
+  private contacts: Map<string, Contact>;
 
   constructor() {
     this.users = new Map();
+    this.contacts = new Map();
   }
 
+  // User Methods
   async getUser(id: string): Promise<User | undefined> {
     return this.users.get(id);
   }
 
   async getUserByUsername(username: string): Promise<User | undefined> {
     return Array.from(this.users.values()).find(
-      (user) => user.username === username,
+      (user) => user.username === username
     );
   }
 
@@ -32,6 +35,19 @@ export class MemStorage implements IStorage {
     const user: User = { ...insertUser, id };
     this.users.set(id, user);
     return user;
+  }
+
+  // Contact Methods
+  async getContact(id: string): Promise<Contact | undefined> {
+    return this.contacts.get(id);
+  }
+
+  async createContact(insertContact: InsertContact): Promise<Contact> {
+    const id = randomUUID();
+    const createdAt = new Date();
+    const contact: Contact = { ...insertContact, id, createdAt };
+    this.contacts.set(id, contact);
+    return contact;
   }
 }
 
